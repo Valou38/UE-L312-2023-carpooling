6aa066b44d64f5d110f9b0913225c19d41535eff
Valentine <89517413+Valou38@users.noreply.github.com>
2023-11-26 18:50:00 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,13 +1,129 @@
-CREATE TABLE `users` (
-  `id` int AUTO_INCREMENT NOT NULL,
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 26 nov. 2023 à 09:47
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `carpooling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `carpoolad`
+--
+
+DROP TABLE IF EXISTS `carpoolad`;
+CREATE TABLE IF NOT EXISTS `carpoolad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `carid` int NOT NULL,
+  `description` text NOT NULL,
+  `dateandtime` datetime NOT NULL,
+  `departurelocation` varchar(255) NOT NULL,
+  `destination` varchar(255) NOT NULL,
+  `availableseats` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `carid` (`carid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE IF NOT EXISTS `cars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `year` year NOT NULL,
+  `mileage` varchar(7) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `cars`
+--
+
+INSERT INTO `cars` (`id`, `brand`, `model`, `year`, `mileage`) VALUES
+(2, 'AUDI', 'A6', 2001, '300');
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `adid` int NOT NULL,
+  `userid` int NOT NULL,
+  `dateandtime` datetime NOT NULL,
+  `reservedseats` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `adid` (`adid`),
+  KEY `userid` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `birthday` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `users`
+--
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
-(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
-(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
-(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+(1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08 00:00:00'),
+(2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08 00:00:00'),
+(3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 09:44:46');
+
+--
+-- Contraintes pour les tables déchargées
+--
+--
+-- Contraintes pour la table `carpoolad`
+--
+ALTER TABLE `carpoolad`
+  ADD CONSTRAINT `carpoolad_ibfk_1` FOREIGN KEY (`carid`) REFERENCES `cars` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`adid`) REFERENCES `carpoolad` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
