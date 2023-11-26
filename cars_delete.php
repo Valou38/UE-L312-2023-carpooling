a81632076169918bf62d668ac7fe24f3149f53b6
Siham Charef <siham.charef@mssiweb.com>
2023-11-26 14:17:02 +0100

    branch-siham-1: Create a delete view for cars

---
+++ cars_delete.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Controllers\CarsController;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$controller = new CarsController();
+echo $controller->deleteCar();
+
+?>
+
+<p>Supression d'un véhicule</p>
+<form method="post" action="cars_delete.php" name ="carDeleteForm">
+    <label for="id">Id :</label>
+    <input type="text" name="id">
+    <br />
+    <input type="submit" value="Supprimer un véhicule">
+</form>
