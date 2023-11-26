8bf4ddaf6abd59d48a6db6386ad9dfea2dc8ee33
Siham Charef <siham.charef@mssiweb.com>
2023-11-26 14:21:05 +0100

    branch-siham-1: Create a php index file (for buttons to access the forms)

---
+++ index.php
@@ -0,0 +1,11 @@
+<a href="users_create.php">User Create</a></br>
+<a href="users_read.php">Users Read</a></br>
+<a href="users_update.php">User Update</a></br>
+<a href="users_delete.php">User Delete</a>
+
+<hr>
+
+<a href="cars_create.php">Car Create</a></br>
+<a href="cars_read.php">Cars Read</a></br>
+<a href="cars_update.php">Cars Update</a></br>
+<a href="cars_delete.php">Car Delete</a></br>
