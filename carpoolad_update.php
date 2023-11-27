af26a2ed49ce287b2ccdc5921ae3d0958fab658f
Valentine <89517413+Valou38@users.noreply.github.com>
2023-11-27 22:51:04 +0100

    Edit file for update carpoolad

--- carpoolad_update.php
+++ carpoolad_update.php
@@ -15,25 +15,25 @@ $adService = new \App\Services\CarpooladService();
 <p>Mise à jour d'une annonce</p>
 <form method="post" action="carpoolad_update.php" name ="carpooladUpdateForm">
     <label for="id">Id :</label>
-    <select name="carid">
-    <option value="">--Choisissez une annonce--</option>
-    <?php
+    <select name="id">
+        <option value="">--Choisissez une annonce--</option>
+        <?php
         $ads = $adService->getCarpoolad();
         foreach ($ads as $ad) {
             echo "<option value='{$ad->getId()}'>{$ad->getId()}</option>";
         }
-    ?>
+        ?>
     </select>
     <br />
     <label for="carid">ID du véhicule:</label>
     <select name="carid">
-    <option value="">--Choisissez un véhicule--</option>
-    <?php
+        <option value="">--Choisissez un véhicule--</option>
+        <?php
         $cars = $carService->getCars();
-            foreach ($cars as $car) {
-        echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
+        foreach ($cars as $car) {
+            echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
         }
-    ?>
+        ?>
     </select>
     <br />
     <label for="description">Description de l'annonce : </label>
@@ -43,20 +43,20 @@ $adService = new \App\Services\CarpooladService();
     <input type="datetime-local" name="dateandtime">
     <br />
     <label for="departurelocation">Lieu de départ :</label>
-    <input type="text" name="départurelocation">
+    <input type="text" name="departurelocation">
     <br />
     <label for="destination">Lieu d'arrivée :</label>
     <input type="text" name="destination">
     <br />
     <label for="availableseats">Nombre de sièges disponibles : </label>
-        <select name="availableseats">
+    <select name="availableseats">
         <option value="">--Nombre de sièges--</option>
         <?php
-          for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             echo "<option value='$i'>$i</option>";
-          }
+        }
         ?>
-        </select>
+    </select>
     <br />
     <input type="submit" value="Modifier l'annonce">
 </form>
