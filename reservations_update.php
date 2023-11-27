ba59422546ab1ecfbf07653e71d32c4d5748971b
Siham Charef <siham.charef@mssiweb.com>
2023-11-27 10:11:10 +0100

    branch-siham-1: Update reservations_update.php view (type datetime-local)

--- reservations_update.php
+++ reservations_update.php
@@ -35,7 +35,7 @@ echo $controller->updateReservation();
     </select>
     <br/>
     <label for="dateandtime">Date et heure:</label>
-    <input type="datetime" name="dateandtime">
+    <input type="datetime-local" name="dateandtime">
     <br>
     <label for="reservedseats">Nombre de siège réservé:</label>
     <select name="reservedseats">
