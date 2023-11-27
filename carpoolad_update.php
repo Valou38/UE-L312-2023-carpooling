859f39933283bac99611ae0e622f3040933105b8
Valentine <89517413+Valou38@users.noreply.github.com>
2023-11-27 10:01:55 +0100

    Update carpoolad_update.php

--- carpoolad_update.php
+++ carpoolad_update.php
@@ -30,7 +30,17 @@ echo $controller->updateCarpoolad();
     <input type="text" name="destination">
     <br />
     <label for="availableseats">Nombre de sièges disponibles : </label>
-    <input type="int" name="availableseats">
+        <select type="int" name="availableseats">
+          <option value="">--Please choose an option--</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+        </select>
     <br />
     <input type="submit" value="Modifier l'annonce">
 </form>
