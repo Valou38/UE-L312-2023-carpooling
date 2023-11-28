d96c2250d8aa92f93867d2abdc626a51032ec019
Valentine <89517413+Valou38@users.noreply.github.com>
2023-11-28 23:01:00 +0100

    Update from sql errors

--- tables.sql
+++ tables.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : mar. 28 nov. 2023 à 22:24
--- Version du serveur : 10.4.28-MariaDB
--- Version de PHP : 8.2.4
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 28 nov. 2023 à 16:15
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -21,13 +21,13 @@ SET time_zone = "+00:00";
 -- Base de données : `carpooling`
 --
 
--- --------------------------------------------------------
 
 --
 -- Structure de la table `carpoolad`
 --
 
-CREATE TABLE `carpoolad` (
+DROP TABLE IF EXISTS `carpoolad`;
+CREATE TABLE IF NOT EXISTS `carpoolad` (
   `id` int(11) NOT NULL,
   `users_cars_id` int(11) NOT NULL,
   `description` text NOT NULL,
@@ -35,9 +35,16 @@ CREATE TABLE `carpoolad` (
   `departurelocation` varchar(255) NOT NULL,
   `destination` varchar(255) NOT NULL,
   `availableseats` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
+
+-- Index pour la table `carpoolad`
+--
+ALTER TABLE `carpoolad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `carpoolad_ibfk_1` (`users_cars_id`);
+
+
 
 --
 -- Structure de la table `cars`
@@ -51,53 +58,71 @@ CREATE TABLE `cars` (
   `mileage` varchar(7) NOT NULL,
   `color` varchar(255) NOT NULL,
   `nbrSlots` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 
+
+-- Index pour la table `cars`
 --
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+
 -- Déchargement des données de la table `cars`
 --
-
 INSERT INTO `cars` (`id`, `brand`, `model`, `year`, `mileage`, `color`, `nbrSlots`) VALUES
-(2, 'AUDI', 'A6', '2001', '300', '', 0),
-(9, 'Twingo', 'Sport', '2023', '50000', '', 0);
+(2, 'AUDI', 'A6', '2001', '300', 'blue', 5),
+(9, 'Twingo', 'Sport', '2023', '50000', 'red', 4);
 
--- --------------------------------------------------------
 
 --
 -- Structure de la table `reservation`
 --
 
-CREATE TABLE `reservation` (
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
   `id` int(11) NOT NULL,
   `adid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
   `reservedseats` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
+
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reservation_ibfk_1` (`adid`),
+  ADD KEY `reservation_ibfk_2` (`userid`);
+
 
 --
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `birthday` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
 
+
+-- Index pour la table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
 -- Déchargement des données de la table `users`
 --
-
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08 00:00:00'),
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08 00:00:00'),
 (3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08 09:44:46');
 
--- --------------------------------------------------------
+
 
 --
 -- Structure de la table `users_cars`
@@ -107,40 +132,9 @@ CREATE TABLE `users_cars` (
   `id` int(11) NOT NULL,
   `car_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `carpoolad`
---
-ALTER TABLE `carpoolad`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `carpoolad_ibfk_1` (`users_cars_id`);
-
---
--- Index pour la table `cars`
---
-ALTER TABLE `cars`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `reservation_ibfk_1` (`adid`),
-  ADD KEY `reservation_ibfk_2` (`userid`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Index pour la table `users_cars`
 --
 ALTER TABLE `users_cars`
@@ -148,47 +142,13 @@ ALTER TABLE `users_cars`
   ADD KEY `ID_user` (`user_id`),
   ADD KEY `ID_car` (`car_id`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
 
---
--- AUTO_INCREMENT pour la table `carpoolad`
---
-ALTER TABLE `carpoolad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
---
--- AUTO_INCREMENT pour la table `cars`
---
-ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT pour la table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT pour la table `users_cars`
---
-ALTER TABLE `users_cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
 
 --
 -- Contraintes pour la table `carpoolad`
 --
+
 ALTER TABLE `carpoolad`
   ADD CONSTRAINT `carpoolad_ibfk_1` FOREIGN KEY (`users_cars_id`) REFERENCES `users_cars` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
