c709f0ae3ec2a22f7ae2b25046936f472fbed7a1
Siham - Mssiweb <siham.charef@etu.unilim.fr>
2023-11-29 20:29:30 +0100

    branch-siham-2: CSS ads view

--- carpoolad_create.php
+++ carpoolad_create.php
@@ -9,43 +9,59 @@ echo $controller->createCarpoolad();
 
 $carService = new \App\Services\CarsService();
 ?>
-
-
-<p>Création d'une annonce</p>
-<form method="post" action="carpoolad_create.php" name ="carpooladCreateForm">
-    <label for="carid">Choisir une voiture :</label>
-    <select name="carid">
-          <option value="">--Choisissez l'ID de votre voiture--</option>
-    <?php
-        $cars = $carService->getCars();
-        foreach ($cars as $car) {
-            echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
-        }
-    ?>
-    </select>
-    <br />
-    <label for="description">Description de l'annonce</label>
-    <input type="text" name="description">
-    <br />
-    <label for="dateandtime">Jour et heure du départ</label>
-    <input type="datetime-local" name="dateandtime">
-    <br />
-    <label for="departurelocation">Lieu de départ</label>
-    <input type="text" name="departurelocation">
-    <br />
-    <label for="destination">Lieu d'arrivée</label>
-    <input type="text" name="destination">
-    <br />
-    <label for="availableseats">Nombre de sièges disponibles</label>
-        <select name="availableseats">
-        <option value="">--Nombre de sièges--</option>
-        <?php
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<option value='$i'>$i</option>";
-            }
-        ?>
-        </select>
-    <br />
-
-    <input type="submit" value="Créer une annonce">
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="/UE-L312-2023-carpooling/assets/css/styles.css">
+</head>
+<body>
+    <h1>Création d'une annonce</h1>
+    <div class="form-container">
+        <form method="post" action="carpoolad_create.php" name="carpooladCreateForm">
+            <div class="form-field">
+                <label for="carid">Choisir une voiture :</label>
+                <select name="carid">
+                    <option value="">--Choisissez l'ID de votre voiture--</option>
+                    <?php
+                    $cars = $carService->getCars();
+                    foreach ($cars as $car) {
+                        echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-field">
+                <label for="description">Description de l'annonce</label>
+                <input type="text" name="description">
+            </div>
+            <div class="form-field">
+                <label for="dateandtime">Jour et heure du départ</label>
+                <input type="datetime-local" name="dateandtime">
+            </div>
+            <div class="form-field">
+                <label for="departurelocation">Lieu de départ</label>
+                <input type="text" name="departurelocation">
+            </div>
+            <div class="form-field">
+                <label for="destination">Lieu d'arrivée</label>
+                <input type="text" name="destination">
+            </div>
+            <div class="form-field">
+                <label for="availableseats">Nombre de sièges disponibles</label>
+                <select name="availableseats">
+                    <option value="">--Nombre de sièges--</option>
+                    <?php
+                    for ($i = 1; $i <= 10; $i++) {
+                        echo "<option value='$i'>$i</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-field">
+                <input type="submit" value="Créer une annonce">
+            </div>
+        </form>
+    </div>
+</body>
--- carpoolad_update.php
+++ carpoolad_update.php
@@ -11,52 +11,71 @@ $carService = new \App\Services\CarsService();
 $adService = new \App\Services\CarpooladService();
 
 ?>
-
-<p>Mise à jour d'une annonce</p>
-<form method="post" action="carpoolad_update.php" name ="carpooladUpdateForm">
-    <label for="id">Id :</label>
-    <select name="id">
-        <option value="">--Choisissez une annonce--</option>
-        <?php
-        $ads = $adService->getCarpoolad();
-        foreach ($ads as $ad) {
-            echo "<option value='{$ad->getId()}'>{$ad->getId()}</option>";
-        }
-        ?>
-    </select>
-    <br />
-    <label for="carid">ID du véhicule:</label>
-    <select name="carid">
-        <option value="">--Choisissez un véhicule--</option>
-        <?php
-        $cars = $carService->getCars();
-        foreach ($cars as $car) {
-            echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
-        }
-        ?>
-    </select>
-    <br />
-    <label for="description">Description de l'annonce : </label>
-    <input type="text" name="description">
-    <br />
-    <label for="dateandtime">Jour et heure du départ :</label>
-    <input type="datetime-local" name="dateandtime">
-    <br />
-    <label for="departurelocation">Lieu de départ :</label>
-    <input type="text" name="departurelocation">
-    <br />
-    <label for="destination">Lieu d'arrivée :</label>
-    <input type="text" name="destination">
-    <br />
-    <label for="availableseats">Nombre de sièges disponibles : </label>
-    <select name="availableseats">
-        <option value="">--Nombre de sièges--</option>
-        <?php
-        for ($i = 1; $i <= 10; $i++) {
-            echo "<option value='$i'>$i</option>";
-        }
-        ?>
-    </select>
-    <br />
-    <input type="submit" value="Modifier l'annonce">
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="/UE-L312-2023-carpooling/assets/css/styles.css">
+</head>
+<body>
+    <h1>Mise à jour d'une annonce</h1>
+    <div class="form-container">
+        <form method="post" action="carpoolad_update.php" name="carpooladUpdateForm">
+            <div class="form-field">
+                <label for="id">Id :</label>
+                <select name="id">
+                    <option value="">--Choisissez une annonce--</option>
+                    <?php
+                    $ads = $adService->getCarpoolad();
+                    foreach ($ads as $ad) {
+                        echo "<option value='{$ad->getId()}'>{$ad->getId()}</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-field">
+                <label for="carid">ID du véhicule:</label>
+                <select name="carid">
+                    <option value="">--Choisissez un véhicule--</option>
+                    <?php
+                    $cars = $carService->getCars();
+                    foreach ($cars as $car) {
+                        echo "<option value='{$car->getId()}'>{$car->getId()}</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-field">
+                <label for="description">Description de l'annonce : </label>
+                <input type="text" name="description">
+            </div>
+            <div class="form-field">
+                <label for="dateandtime">Jour et heure du départ :</label>
+                <input type="datetime-local" name="dateandtime">
+            </div>
+            <div class="form-field">
+                <label for="departurelocation">Lieu de départ :</label>
+                <input type="text" name="departurelocation">
+            </div>
+            <div class="form-field">
+                <label for="destination">Lieu d'arrivée :</label>
+                <input type="text" name="destination">
+            </div>
+            <div class="form-field">
+                <label for="availableseats">Nombre de sièges disponibles : </label>
+                <select name="availableseats">
+                    <option value="">--Nombre de sièges--</option>
+                    <?php
+                    for ($i = 1; $i <= 10; $i++) {
+                        echo "<option value='$i'>$i</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-field">
+                <input type="submit" value="Modifier l'annonce">
+            </div>
+        </form>
+    </div>
+</body>
